95f8d8cbd4de1613ff47885e7db6980b394f1326
zhangChao <1148563917@qq.com>
2018-01-11 16:30:57 +0800

    zc

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ REPLACE = process.argv[3] ? process.argv[4] : 'false';
 
 console.log('执行的参数', PATH, SIZE, REPLACE);
 
+// 图片处理函数
+function resize(item) {
+  var fT = fileType(fs.readFileSync(item));
+  if (fT.mime === 'image/png' || fT.mime === 'image/jpeg' || fT.mime === 'image/gif') {
+    console.log('%s 文件开始转化...', item);
+    let outFile = item.replace(`.${fT.ext}`, `-min.${fT.ext}`);
+    sharp(item).resize(SIZE).toFile(outFile, function(err) {
+      if (err) {
+        console.log('%s 文件转化失败', item);
+        return err;
+      }
+      console.log('%s 文件转化完成', item);
+      // 删除原有图片
+      if (REPLACE === 'true') {
+        console.log('删除原有图片', outFile, item);
+        fs.rename(outFile, item, function(err) {
+          if(err) {
+            console.error('删除原有图片失败', item, err);
+            return err;
+          }
+        });
+      }
+    });
+  }
+}
+
 /**
  * 图片处理的逻辑
  * 功能：1. 遍历目录下的所有的未处理的图片并进行压缩 2. 是否删除原来的图片
@@ -22,7 +48,7 @@ console.log('执行的参数', PATH, SIZE, REPLACE);
  * @param {string} SIZE - 命令的第二个参数，图片的大小
  * @param {string} REPLACE - 命令的第三个参数，是否删除原来的图片
  */
-function getImages(path) {
+function resizeImages(path) {
   fs.readdir(path, function(err, files) {
     if(err) {
       console.error('Task Error! Try again!');
@@ -38,30 +64,9 @@ function getImages(path) {
             return err;
           }
           if (result.isDirectory()) {
-            getImages(item, SIZE);
+            resizeImages(item, SIZE);
           } else if (result.isFile()) {
-            var fT = fileType(fs.readFileSync(item));
-            if (fT.mime === 'image/png' || fT.mime === 'image/jpeg' || fT.mime === 'image/gif') {
-              console.log('%s 文件开始转化...', item);
-              let outFile = item.replace(`.${fT.ext}`, `-min.${fT.ext}`);
-              sharp(item).resize(SIZE).toFile(outFile, function(err) {
-                if (err) {
-                  console.log('%s 文件转化失败', item);
-                  return err;
-                }
-                console.log('%s 文件转化完成', item);
-                // 删除原有图片
-                if (REPLACE === 'true') {
-                  console.log('sssss', outFile, item);
-                  fs.rename(outFile, item, function(err) {
-                    if(err) {
-                      console.error('删除原有图片失败', item, err);
-                      return err;
-                    }
-                  });
-                }
-              });
-            }
+            resize(item);
           }
         });
       }
@@ -69,6 +74,19 @@ function getImages(path) {
   });
 }
 
+// 处理单张图片
+function resizeImage(path) {
+  resize(path);
+}
+
 if (PATH) {
-  getImages(PATH, SIZE);
+  fs.stat(PATH, function(err, result) {
+    if (result.isDirectory()) {
+      // 如果是目录则进行批量处理
+      resizeImages(PATH, SIZE);
+    } else {
+      // 如果是单张图片
+      resizeImage(PATH, SIZE);
+    }
+  });
 }
