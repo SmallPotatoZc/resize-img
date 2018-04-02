0d7f9968ae011ded1f32ee7c174775cfdfcf87e4
zhangChao <1148563917@qq.com>
2018-04-02 14:05:47 +0800

    macos

--- index.js
+++ index.js
@@ -3,35 +3,95 @@ const sizeOf = require('image-size');
 const fileType = require('file-type');
 const sharp = require('sharp');
 const fs = require('fs');
+const path = require('path');
 const process = require('process');
 
-let PATH = ''; // 处理的路径
-let SIZE = 1024; // 处理图片的最大尺寸
-let REPLACE = 'false';
+// 程序的执行路径
+const CURPATH = process.cwd();
 
-PATH = process.argv[2];
-SIZE = process.argv[3] ? Number(process.argv[3]) : 1024;
-REPLACE = process.argv[3] ? process.argv[4] : 'false';
+const param_config = {
+  name: 'resize',
+  description: '图片压缩',
+  options: [{
+    name: 'path',
+    shortcut: 'p',
+    default: '',
+    description: '目标路径'
+  }, {
+    name: 'watch',
+    shortcut: 'w',
+    default: false,
+    description: '是否监听目标路径'
+  }, {
+    name: 'size',
+    shortcut: 's',
+    default: 1024,
+    description: '图片压缩尺寸'
+  }, {
+    name: 'replace',
+    shortcut: 'r',
+    default: false,
+    description: '是否替换原有图片'
+  }]
+}
 
-console.log('执行的参数', PATH, SIZE, REPLACE);
+const params = require('parameters')(param_config);
+const args = params.parse();
+
+SIZE = args.size ? Number(args.size) : 1024;
+REPLACE = args.replace ? args.replace : 'false';
+
+// 开始执行
+if (!args.help) {
+  const _path = path.resolve(CURPATH, `${args.path}`); // 当前路径
+  let _continus = true;
+  if (!args.watch) {
+    // 判断是否为有效路径
+    if (fs.existsSync(_path)) {
+      if (fs.lstatSync(_path).isDirectory()) {
+        resizeImages(_path);
+      } else {
+        resizeImage(_path);
+      }
+    } else {
+      throw new Error('路径无效');
+    }
+  } else {
+    let _dir = _path;
+    if (!fs.lstatSync(_path).isDirectory()) {
+      _dir = path.dirname(_path);
+    }
+    fs.watch(_dir, {recursive:true}, (eventType, filename) => {
+      if (filename) {
+        let _tar = `${_dir}/${filename}`;
+        if(fs.existsSync(_tar) && !/-min\./g.test(filename)){
+          console.log('ddddd', _tar);
+          resizeImage(_tar);
+        }
+      }
+    });
+  }
+}else{
+  console.log(params.help());
+}
 
 // 图片处理函数
 function resize(item) {
   var fT = fileType(fs.readFileSync(item));
-  if (fT.mime === 'image/png' || fT.mime === 'image/jpeg' || fT.mime === 'image/gif') {
+  if (fT && (fT.mime === 'image/png' || fT.mime === 'image/jpeg' || fT.mime === 'image/gif')) {
     console.log('%s 文件开始转化...', item);
     let outFile = item.replace(`.${fT.ext}`, `-min.${fT.ext}`);
-    sharp(item).resize(SIZE).toFile(outFile, function(err) {
+    sharp(item).resize(SIZE).toFile(outFile, function (err) {
       if (err) {
         console.log('%s 文件转化失败', item);
         return err;
       }
       console.log('%s 文件转化完成', item);
       // 删除原有图片
-      if (REPLACE === 'true') {
+      if (REPLACE == true) {
         console.log('删除原有图片', outFile, item);
-        fs.rename(outFile, item, function(err) {
-          if(err) {
+        fs.rename(outFile, item, function (err) {
+          if (err) {
             console.error('删除原有图片失败', item, err);
             return err;
           }
@@ -41,25 +101,18 @@ function resize(item) {
   }
 }
 
-/**
- * 图片处理的逻辑
- * 功能：1. 遍历目录下的所有的未处理的图片并进行压缩 2. 是否删除原来的图片
- * @param {string} PATH - 命令的第一个参数，图片的处理路径
- * @param {string} SIZE - 命令的第二个参数，图片的大小
- * @param {string} REPLACE - 命令的第三个参数，是否删除原来的图片
- */
 function resizeImages(path) {
-  fs.readdir(path, function(err, files) {
-    if(err) {
+  fs.readdir(path, function (err, files) {
+    if (err) {
       console.error('Task Error! Try again!');
       return err;
     }
     let minReg = /-min./;
-    _.map(files, function(item) {
+    _.map(files, function (item) {
       if (!minReg.test(item)) {
         item = path + '/' + item;
-        fs.stat(item, function(err, result) {
-          if(err) {
+        fs.stat(item, function (err, result) {
+          if (err) {
             console.error('打开文件时出现错误');
             return err;
           }
@@ -77,16 +130,4 @@ function resizeImages(path) {
 // 处理单张图片
 function resizeImage(path) {
   resize(path);
-}
-
-if (PATH) {
-  fs.stat(PATH, function(err, result) {
-    if (result.isDirectory()) {
-      // 如果是目录则进行批量处理
-      resizeImages(PATH, SIZE);
-    } else {
-      // 如果是单张图片
-      resizeImage(PATH, SIZE);
-    }
-  });
 }
